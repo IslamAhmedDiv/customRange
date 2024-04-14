61faab628d757e4f5b0cab9ef907561f3954cd0d
IslamAhmedDiv <IslamDev.sj@gmail.com>
2024-04-14 15:07:23 +0200

    drag thumb v5

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
 const thumb = document.getElementById("thumb")
+const slider = document.getElementById("slider")
+const width = slider.offsetWidth
 
-let startX;
-thumb.ondragstart = (pos)=>{
-    startX = pos.screenX
+let start_x, moving_x, end_x, movement
+
+let mouseDown = (pos)=>{
+    start_x = pos.screenX
+    console.log("start_x", pos.screenX)
 }
 
-thumb.ondrag = (pos)=>{
-    const x = pos.screenX
-    const movement = x - startX
+
+let mouseMove = (pos)=>{
+    moving_x = pos.screenX
+    movement = moving_x - start_x
+    // console.log("moving_x", moving_x)
     thumb.style.left = `${movement}px`
-    // thumb.style.cursor = "pointer"
 }
 
+let mouseUp = (pos)=>{
+    end_x = pos.screenX
+    console.log("mouseUp")
+
+    // remove events listener
+    thumb.removeEventListener("mousedown", mouseDown)
+    thumb.removeEventListener("mousemove", mouseMove)
+}
+
+
+thumb.addEventListener("mousedown", (pos)=>{
+
+    // call a function on click mouse down
+    mouseDown(pos)
+
+    // call a function on click mouse move
+    thumb.addEventListener("mousemove", mouseMove)
+
+    // call a function on click mouse up and clear events
+    thumb.addEventListener("mouseup", (pos)=>{
+        console.log("removed")
+        mouseUp(pos)
+    })
+
+    // thumb.addEventListener("mouseleave", (pos)=>{
+    //     console.log("removed")
+    //     mouseUp(pos)
+    // })
+
+})
 
 
 
