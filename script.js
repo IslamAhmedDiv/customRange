77d116aa52581232628a82e0c8bd00a73cdef50d
IslamAhmedDiv <IslamDev.sj@gmail.com>
2024-04-14 20:32:26 +0200

    min and max values and starting from previous value

--- script.js
+++ script.js
@@ -1,53 +1,74 @@
 const thumb = document.getElementById("thumb")
 const slider = document.getElementById("slider")
+const body = document.body
 const width = slider.offsetWidth
+let start_x, moving_x, movement, place
 
-let start_x, moving_x, end_x, movement
-
-let mouseDown = (pos)=>{
-    start_x = pos.screenX
-    console.log("start_x", pos.screenX)
+// get position of an element
+function positionElement(elm){
+    let clickEvent = new MouseEvent("click", {});
+    let posX, posY
+    elm.addEventListener("click", (p)=>{
+        posX = p.offsetX
+        posY = p.offsetY
+    })
+    elm.dispatchEvent(clickEvent);
+    return {posX, posY}
 }
+let slider_pos_x = (positionElement(slider).posX) * -1
 
+// events to the thumb
+let mouseDown = ()=>{
+    // let screen_x = pos.screenX
+    // let point_slider = screen_x - slider_pos_x
+}
 
 let mouseMove = (pos)=>{
     moving_x = pos.screenX
-    movement = moving_x - start_x
-    // console.log("moving_x", moving_x)
+    movement = moving_x - slider_pos_x
+    if(movement < 0 ){movement = 0
+    }else if(movement > width){movement = width}
+
     thumb.style.left = `${movement}px`
+
+    // set value to the slider
+    place = (movement / width ) * 100
+    slider.dataset.value = place
+    body.style.cursor = "grab"
 }
 
-let mouseUp = (pos)=>{
-    end_x = pos.screenX
-    console.log("mouseUp")
-
+let mouseUp = ()=>{
     // remove events listener
+    body.style.cursor = "auto"
     thumb.removeEventListener("mousedown", mouseDown)
-    thumb.removeEventListener("mousemove", mouseMove)
+    body.removeEventListener("mousemove", mouseMove)
 }
 
 
-thumb.addEventListener("mousedown", (pos)=>{
-
-    // call a function on click mouse down
-    mouseDown(pos)
+thumb.addEventListener("mousedown", ()=>{
 
     // call a function on click mouse move
-    thumb.addEventListener("mousemove", mouseMove)
+    body.addEventListener("mousemove", mouseMove)
 
     // call a function on click mouse up and clear events
-    thumb.addEventListener("mouseup", (pos)=>{
-        console.log("removed")
-        mouseUp(pos)
-    })
-
-    // thumb.addEventListener("mouseleave", (pos)=>{
-    //     console.log("removed")
-    //     mouseUp(pos)
-    // })
+    body.addEventListener("mouseup", mouseUp)
+    body.addEventListener("mouseleave",mouseUp)
+    thumb.addEventListener("dragstart",mouseMove)
 
 })
 
 
+let mouseDown_thumb = (pos)=>{
+    start_x = pos.offsetX
+    thumb.style.left = `${start_x}px`
+
+    // call a function on click mouse move
+    body.addEventListener("mousemove", mouseMove)
+    body.addEventListener("mouseup", mouseUp)
+    body.addEventListener("mouseleave",mouseUp)
+    thumb.addEventListener("dragstart",mouseMove)
+}
+
+slider.addEventListener("mousedown", mouseDown_thumb)
 
 
